3f708670b09809c8567a7f5676e01715b085b61d
ignaferdel@gmail.com <ignaferdel@gmail.com>
2023-08-18 21:45:32 -0400

    ch15

--- books/views.py
+++ books/views.py
@@ -26,6 +26,7 @@ class SearchResultsListView(ListView):
     template_name = "books/search_results.html"
 
     def get_queryset(self):
+        query =  self.request.GET.get("q")
         return Book.objects.filter(
-            Q(title__icontains="beginners") | Q(title__icontains="api")
+            Q(title__icontains=query) | Q(author__icontains=query)
         )
